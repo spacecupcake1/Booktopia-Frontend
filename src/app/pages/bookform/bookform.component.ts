a154e62d5842952a16e64d4c6f8c0d0abe796e52
unknown <jregani@yahoo.com>
2024-05-30 15:55:08 +0200

    working on bookform

--- src/app/pages/bookform/bookform.component.ts
+++ src/app/pages/bookform/bookform.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { Book } from '../../data/book';
+import { BookService } from '../../service/book.service';
 
 @Component({
   selector: 'app-bookform',
   templateUrl: './bookform.component.html',
   styleUrl: './bookform.component.css'
 })
-export class BookformComponent {
+export class BookformComponent implements OnInit {
+
+  book = new Book();
+  public bookForm: FormGroup;
+
+  constructor(
+    private bookService: BookService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.bookForm = this.formBuilder.group({
+      firstname: new FormControl(''),
+      lastname: new FormControl(''),
+      birthdate: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      this.bookService.getOne(+id).subscribe(book => {
+        this.book = book;
+        this.bookForm.patchValue(book);
+      }, error => {
+        console.error('Error fetching book', error);
+      });
+    } else {
+      this.bookForm.patchValue(this.book);
+    }
+  }
+
+  async back() {
+    await this.router.navigate(['booktable']);
+  }
+
+  async save(formData: any) {
+    this.book = Object.assign(this.book, formData);
+
+    if (this.book.id) {
+      this.bookService.update(this.book).subscribe({
+        next: () => {
+          this.back();
+        },
+        error: (err) => {
+          console.error('Error updating book', err);
+        }
+      });
+    } else {
+      this.bookService.save(this.book).subscribe({
+        next: () => {
+          this.back();
+        },
+        error: (err) => {
+          console.error('Error saving book', err);
+        }
+      });
+    }
+  }
 
 }
--- src/app/pages/booktable/booktable.component.ts
+++ src/app/pages/booktable/booktable.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { Book } from '../../data/book';
+import { BookService } from '../../service/book.service';
 
 @Component({
   selector: 'app-booktable',
   templateUrl: './booktable.component.html',
   styleUrl: './booktable.component.css'
 })
-export class BooktableComponent {
+export class BooktableComponent implements OnInit, AfterViewInit {
+  bookdata = new MatTableDataSource<Book>();
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+
+  constructor(private bookService: BookService, private router: Router) {
+    this.reloadData();
+   }
+
+  displayedColumns: string[] = ['id', 'name', 'author', 'series', 'page', 'releaseDate', 'author', 'award', 'actions'];
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this.bookdata.paginator = this.paginator;
+    }
+  }
+
+  reloadData() {
+    this.bookService.getList().subscribe((books) => {
+      this.bookdata.data = books;
+    });
+  }
+
+  async edit(e: Book) {
+    await this.router.navigate(['bookform', e.id]);
+  }
+
+  async add() {
+    await this.router.navigate(['bookform']);
+  }
+
+  delete(e: Book) {
+        this.bookService.delete(e.id).subscribe({
+          next: response => {
+            console.log(response)
+            if (response.status === 200) {
+              this.reloadData();
+            } 
+          }
+        });
+  }
+
 
 }
+
